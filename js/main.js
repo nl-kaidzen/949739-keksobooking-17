fd6f70409d91569e2f40c949f255a23314c79486
Andrey Khabarow <nl.kaidzen@gmail.com>
2019-06-28 06:17:18 +0300

    modules for common and data.js

--- js/main.js
+++ js/main.js
@@ -1,40 +1,5 @@
 'use strict';
 
-var objectsForRent = [];
-
-/*  Create random number (min, max) */
-var createNumber = function (minNumber, maxNumber) {
-  return (Math.round(minNumber + Math.random() * (maxNumber - minNumber)));
-};
-
-/*  ____________________________MOCKUPS DATA___________________________*/
-// createNumber function from WINDOW;
-// objectsForRent from WINDOW;
-var OFFER_TYPE = ['palace', 'flat', 'house', 'bungalo'];
-var SCREEN_WIDTH = 1200;
-
-/*  Create data for rent object */
-var createObjectForRent = function (i) {
-  return {
-    author: {
-      avatar: 'img/avatars/user0' + (i + 1) + '.png'
-    },
-    offer: {
-      type: OFFER_TYPE[createNumber(0, 3)]
-    },
-    location: {
-      x: createNumber(0, (SCREEN_WIDTH)),
-      y: createNumber(130, 630)
-    }
-  };
-};
-
-/*  Generate array of objects */
-for (var index = 0; index < 8; index++) {
-  objectsForRent.push(createObjectForRent(index));
-}
-
-
 /*  ----------- MAP LOGIC ------------  */
 
 var mapForPin = document.querySelector('.map');
@@ -143,10 +108,10 @@ var changePinData = function (newPinElement, objectForRentData) {
 };
 /*  Paint new Pins to Map*/
 var paintPin = function () {
-  for (var i = 0; i < objectsForRent.length; i++) {
+  for (var i = 0; i < window.common.objectsForRent.length; i++) {
     var newPinElement = pinButton.cloneNode(true);
     addPinToMap(newPinElement);
-    changePinData(newPinElement, objectsForRent[i]);
+    changePinData(newPinElement, window.common.objectsForRent[i]);
   }
 };
 
