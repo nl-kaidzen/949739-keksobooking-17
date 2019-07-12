ca9a79a5847277c7c684cbac3630b62b21ef73d6
Андрей Валерьевич Хабаров <@fischer.roscepr.ru>
2019-07-12 17:21:11 +0700

    Realization for show features of object at card

--- js/card.js
+++ js/card.js
@@ -2,6 +2,7 @@
 
   window.card.changeCardData = function (object) {
     //  VARIABLES
+    var object = window.common.objectsForRent[1];
     var card = document.querySelector('.popup');
     var cardUserPic = card.querySelector('.popup__avatar');
     var cardCloseBtn = card.querySelector('.popup__close');
@@ -19,18 +20,37 @@
     var cardCheckTime = card.querySelector('.popup__text--time');
     var cardHouseFeatures = card.querySelector('.popup__features');
     //  HOUSE FEATURES
-    var houseWifi = cardHouseFeatures.querySelector('.popup__feature--wifi');
+    var FeaturesClassMap = {
+      "wifi":  'popup__feature--wifi',
+      "dishwasher":  'popup__feature--dishwasher',
+      "parking":  'popup__feature--parking',
+      "washer":  'popup__feature--washer',
+      "elevator":  'popup__feature--elevator',
+      "conditioner":  'popup__feature--conditioner'
+    };
+    var featuresList = Array.from(cardHouseFeatures.children);
+    console.log(featuresList);
+    featuresList.forEach (function (node) {
+      node.remove;
+    })
+    object.offer.features.forEach(function (feature) {
+      var houseFeature = document.createElement('li');
+      houseFeature.className = 'popup__feature';
+      houseFeature.classList.add(FeaturesClassMap[feature]);
+      cardHouseFeatures.appendChild(houseFeature);
+    });
+    /*  ar houseWifi = cardHouseFeatures.querySelector('.popup__feature--wifi');
     var houseDishwasher = cardHouseFeatures.querySelector('.popup__feature--dishwasher');
     var houseParking = cardHouseFeatures.querySelector('.popup__feature--parking');
     var houseWasher = cardHouseFeatures.querySelector('.popup__feature--washer');
     var houseElevator = cardHouseFeatures.querySelector('.popup__feature--elevator');
-    var houseConditioner = cardHouseFeatures.querySelector('.popup__feature--conditioner');
+    var houseConditioner = cardHouseFeatures.querySelector('.popup__feature--conditioner'); */
     // DESCRIPTION
     var cardDescription = card.querySelector('.popup__description');
     //  IMAGES
     var cardImagesContainer = card.querySelector('.popup__photos');
     var houseImage = cardImagesContainer.querySelector('.popup__photo');
-    var object = window.common.objectsForRent[1];
+
 
     /*  Change data */
     cardUserPic.src = object.author.avatar;
@@ -43,4 +63,5 @@
     cardDescription.innerHTML = object.offer.description;
     console.dir(object);
     console.dir(card);
+    console.dir(cardHouseFeatures);
   };
