8dc5e1ad31442738a45174367900c43628819196
Андрей Валерьевич Хабаров <@fischer.roscepr.ru>
2019-07-17 18:02:30 +0700

    Add filter.js

---
+++ js/filter.js
@@ -0,0 +1,19 @@
+'use strict';
+
+//  LISTENERS FOR MAP-FILTER
+/*  variables */
+//  var mapFilters = document.querySelector('.map__filters');
+//  var filterHouseType = mapFilters.querySelector('#housing-type');
+//  var filterHousePrice = mapFilters.querySelector('#housing-price');
+//  var filterHouseRooms = mapFilters.querySelector('#housing-rooms');
+//  var filterHouseGuests = mapFilters.querySelector('#housing-guests');
+
+//  CREATE OBJECT WITH FILTER
+//  var globalFilter = {};
+
+//  var updateGlobalFilter = function () {
+/*  globalFilter.type = filterHouseType.value;
+  globalFilter.price = filterHousePrice.value;
+  globalFilter.rooms = filterHouseRooms.value;
+  globalFilter.guests = filterHouseGuests.value;
+};*/
