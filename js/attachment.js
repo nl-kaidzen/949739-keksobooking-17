034b9afe697e9c5070d8e981c492314268508644
Andrey Khabarow <nl.kaidzen@gmail.com>
2019-08-01 19:14:06 +0300

    Add preview for photos of object @form

--- js/attachment.js
+++ js/attachment.js
@@ -2,12 +2,20 @@
 
 (function () {
   var FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
+  var IMAGE_WIDTH = 70;
 
   var avatarChooser = window.common.adForm.querySelector('#avatar');
   var avatarImg = window.common.adForm.querySelector('#avatar-img');
   var avatarDefault = avatarImg.src;
 
+  var photoChooser = window.common.adForm.querySelector('#images');
+  var photo = window.common.adForm.querySelector('.ad-form__photo');
+  var photoContainer = window.common.adForm.querySelector('.ad-form__photo-container');
+
   var avatarFile;
+  var photoFiles = []; //  Array for files from FileChooser
+  //  var photoCorrect = []; // Array with correct photos for download at server
+  var image; // Image for room photo
 
   //  Clear avatar
   var clearAvatar = function () {
@@ -23,6 +31,7 @@
     });
   };
 
+  // Render avatar
   var renderPreview = function (file, preview) {
     var reader = new FileReader();
 
@@ -39,6 +48,27 @@
       renderPreview(avatarFile, avatarImg);
     }
     window.attachment.avatarFile = undefined;
+  });
+
+  // Render some files
+
+  photoChooser.addEventListener('change', function () {
+    photoFiles = Array.from(photoChooser.files);
+    photoFiles.forEach(function (element) {
+      if (checkType(element)) {
+        if (!image) {
+          image = document.createElement('img');
+          image.width = IMAGE_WIDTH;
+          renderPreview(element, image);
+          photo.appendChild(image);
+        } else {
+          var photoClone = photo.cloneNode(true);
+          image = photoClone.querySelector('img');
+          renderPreview(element, image);
+          photoContainer.appendChild(photoClone);
+        }
+      }
+    });
   });
 
   window.attachment = {
